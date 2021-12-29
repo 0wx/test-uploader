fa686ef40cd9abc315fadd4f61e331c8c423d25b
0wx <hai@0wx.dev>
2021-12-30 03:18:48 +0700

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import express from 'express'
+import axios from 'axios'
+import isUrl from 'is-url'
 import { uploader } from './lib/postimage'
 
 const app = express()
+app.get('/', (req, res) => {
+  axios.get('https://postimg.cc')
+  .then(() => {
+  res.json({ok: true})  
+  }).
+  catch(() => res.json({ok: false}))
+  
+})
 app.get('*', async (req, res) => {
   const url = req.originalUrl.replace('/', '')
+  
   try {
+    if(!isUrl(url)) throw new Error('Not URL')
     const data = await uploader(url)
     res.json({ url: data })
   } catch (error) {
     res.status(500).json({ error })
   }
 })
+console.log(process.env.PORT)
+app.listen(process.env.PORT)
